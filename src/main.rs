e56250bba8cc516244c3c53087eee0a2c594eea1
chris west <c@xvxx.io>
2020-05-10 13:11:33 -0700

    set up us the bomb

--- src/main.rs
+++ src/main.rs
@@ -1,67 +1,132 @@
 use ascii::AsciiString;
-use std::{fs, path::Path};
-use tiny_http::{Method::Get, Response, Server};
+use threadpool::ThreadPool;
+use tiny_http::{Method::Get, Request, Response, Server};
 
-fn main() -> Result<(), std::io::Error> {
+use std::{fs, io, path::Path};
+
+const MAX_WORKERS: usize = 10;
+
+fn main() -> Result<(), io::Error> {
     let host = "0.0.0.0";
     let port = 8000;
-    let server = Server::http(format!("{}:{}", host, port)).unwrap();
+    let addr = format!("{}:{}", host, port);
+    println!("> starting at http://{}", addr);
+    let server = Server::http(addr).unwrap();
+    let pool = ThreadPool::new(MAX_WORKERS);
 
     for request in server.incoming_requests() {
-        let mut body = "404 Not Found".to_string();
-        let mut status = 404;
-        println!("{} {}", request.method(), request.url());
-        match (request.method(), request.url()) {
-            (Get, "/dog") => {
-                status = 200;
-                body = "woof woof".into();
+        pool.execute(move || {
+            if let Err(e) = handle(request) {
+                eprintln!(">> {}", e);
             }
-            (Get, "/cat") => {
-                status = 200;
-                body = "meow".into();
-            }
-            (Get, "/") | (Get, "/index.html") => {
-                status = 200;
-                body = fs::read_to_string("index.html")?;
-            }
-            (Get, path) => {
-                let path = format!(".{}", path.replace("..", "."));
-                if Path::new(&path).exists() {
-                    status = 200;
-                    body = fs::read_to_string(path)?;
-                } else {
-                    status = 404;
-                }
-            }
-            (x, y) => println!("x: {:?}, y: {:?}", x, y),
-        }
-
-        let mut path = Path::new(request.url());
-        if status == 404 {
-            path = Path::new("404.html");
-            body = fs::read_to_string("404.html")?;
-        }
-        let response = Response::from_string(body).with_status_code(status);
-        let response = response.with_header(tiny_http::Header {
-            field: "Content-Type".parse().unwrap(),
-            value: AsciiString::from_ascii(get_content_type(&path)).unwrap(),
         });
-
-        if let Err(e) = request.respond(response) {
-            eprintln!(">> {:?}", e);
-        }
     }
 
     Ok(())
 }
 
-fn get_content_type(path: &Path) -> &'static str {
+fn handle(req: Request) -> Result<(), io::Error> {
+    let mut body = "404 Not Found".to_string();
+    let mut status = 404;
+    let mut content_type = "text/html; charset=utf8";
+
+    println!("{} {}", req.method(), req.url());
+    match (req.method(), req.url()) {
+        (Get, "/") => {
+            status = 200;
+            body = fs::read_to_string("web/index.html")?;
+        }
+        (Get, "/sleep") => {
+            status = 200;
+            body = "Zzzzz...".into();
+            std::thread::sleep(std::time::Duration::from_secs(5));
+        }
+        (Get, "/404") => {
+            status = 404;
+            body = fs::read_to_string("web/404.html")?;
+        }
+
+        (Get, path) => {
+            if let Some(html) = render(path) {
+                status = 200;
+                body = html;
+            } else if let Some(web_path) = web_path(path) {
+                status = 200;
+                body = fs::read_to_string(web_path)?;
+                content_type = get_content_type(path).unwrap_or("text/plain");
+            } else {
+                status = 404;
+                body = fs::read_to_string("web/404.html")?;
+            }
+        }
+
+        (x, y) => println!("x: {:?}, y: {:?}", x, y),
+    }
+
+    let response = Response::from_string(body).with_status_code(status);
+
+    let response = response.with_header(tiny_http::Header {
+        field: "Content-Type".parse().unwrap(),
+        value: AsciiString::from_ascii(content_type).unwrap(),
+    });
+
+    req.respond(response)
+}
+
+/// Render a wiki page to an HTML string.
+/// Wiki pages are stored in the `wiki/` directory as `.md` files.
+fn render(path: &str) -> Option<String> {
+    if let Some(path) = wiki_path(path) {
+        let raw = fs::read_to_string(path).unwrap_or_else(|_| "".into());
+        Some(raw)
+    } else {
+        None
+    }
+}
+
+/// Path of wiki page on disk, if it exists.
+/// Always in the `wiki/` directory.
+/// Eg. wiki_path("Welcome") -> "wiki/welcome.md"
+fn wiki_path(path: &str) -> Option<String> {
+    let path = format!(
+        "./wiki/{}.md",
+        path.to_lowercase()
+            .trim_start_matches('/')
+            .replace("..", ".")
+    );
+    if Path::new(&path).exists() {
+        Some(path)
+    } else {
+        None
+    }
+}
+
+/// Path of asset on disk, if it exists.
+/// Always in the `web/` directory.
+/// Eg web_path("style.css") -> "web/style.css"
+fn web_path(path: &str) -> Option<String> {
+    let path = format!("./web/{}", path.trim_start_matches('/').replace("..", "."));
+    if Path::new(&path).exists() {
+        Some(path)
+    } else {
+        None
+    }
+}
+
+/// Content type for a file on disk. We only look in `web/`.
+fn get_content_type(path: &str) -> Option<&'static str> {
+    let disk_path = web_path(path);
+    if disk_path.is_none() {
+        return None;
+    }
+    let disk_path = disk_path.unwrap();
+    let path = Path::new(&disk_path);
     let extension = match path.extension() {
-        None => return "text/plain",
+        None => return Some("text/plain"),
         Some(e) => e,
     };
 
-    match extension.to_str().unwrap() {
+    Some(match extension.to_str().unwrap() {
         "gif" => "image/gif",
         "jpg" => "image/jpeg",
         "jpeg" => "image/jpeg",
@@ -71,5 +136,5 @@ fn get_content_type(path: &Path) -> &'static str {
         "html" => "text/html; charset=utf8",
         "txt" => "text/plain; charset=utf8",
         _ => "text/plain; charset=utf8",
-    }
+    })
 }
---
+++ wiki/welcome.md
@@ -0,0 +1,21 @@
+# welcome to deadwiki
+
+Hello, and welcome.
+
+- List
+- Two
+- Three
+
+Here's a link: [Help]
+
+Where'd it go?
+
+Try this: https://google.com
+
+AKA [Google](https://google.com)
+
+[Footnotes]
+
+Cool.
+
+[footnotes]: https://github.com
