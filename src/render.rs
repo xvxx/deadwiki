d44347f4deec310abe16c3d45752dffe57c2c73e
chris west <c@xvxx.io>
2020-05-26 16:07:42 -0700

    <details> around sub-directory wiki pages

--- src/render.rs
+++ src/render.rs
@@ -8,6 +8,8 @@ use {
 
 /// Render the index page which lists all wiki pages.
 pub fn index(req: &Request) -> Result<String, io::Error> {
+    let mut folded = "";
+
     Ok(render::layout(
         "deadwiki",
         &format!(
@@ -16,11 +18,29 @@ pub fn index(req: &Request) -> Result<String, io::Error> {
                 "{pages}",
                 &req.page_names()
                     .iter()
-                    .map(|name| format!(
-                        "  <li><a href='{}'>{}</a></li>\n",
-                        name,
-                        wiki_path_to_title(name)
-                    ))
+                    .map(|name| {
+                        let mut prefix = "".to_string();
+                        if let Some(idx) = name.find('/') {
+                            if folded.is_empty() {
+                                folded = &name[..=idx];
+                                prefix = format!("<details><summary>{}</summary>", folded);
+                            } else if folded != &name[..=idx] {
+                                folded = &name[..=idx];
+                                prefix =
+                                    format!("</details><details><summary>{}</summary>", folded);
+                            }
+                        } else if !folded.is_empty() {
+                            prefix = "</details>".to_string();
+                            folded = "";
+                        }
+
+                        format!(
+                            "{}  <li><a href='{}'>{}</a></li>\n",
+                            prefix,
+                            name,
+                            wiki_path_to_title(name)
+                        )
+                    })
                     .collect::<String>()
             )
         ),
