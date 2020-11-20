2f05373f7cdae11e7ae8666977db2c2446e4a6fc
chris west <c@xvxx.io>
2020-11-20 15:01:25 -0800

    spice up js a bit

--- assets/js/deadwiki.js
+++ assets/js/deadwiki.js
@@ -1,13 +1,23 @@
+/* helpers */
+let $ = (sel) => document.querySelector(sel);
+let $$ = (sel) => document.querySelectorAll(sel);
+let GET = (url) => {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.send(null);
+};
+
+/* onload */
 window.onload = () => {
   // focus the element with id=focused
-  var focused = document.querySelector("#focused");
+  var focused = $("#focused");
   if (focused && focused.value == "") focused.focus();
   // or class=focused
-  var focused = document.querySelector(".focused");
+  var focused = $(".focused");
   if (focused && focused.value == "") focused.focus();
 
   // markdown editor
-  var el = document.querySelector("#markdown");
+  var el = $("#markdown");
   if (el) {
     var simplemde = new SimpleMDE({
       autofocus: !focused || focused.value != "",
@@ -27,12 +37,13 @@ window.onload = () => {
   }
 };
 
+/* keydown */
 document.onkeydown = (e) => {
   e = e || window.event || {};
   let name = document.activeElement.tagName;
 
   // editor shortcuts pages that don't have the editor
-  if (document.querySelector("#markdown")) {
+  if ($("#markdown")) {
     // ESC key to go back when editing
     if (e.keyCode == 27) {
       e.preventDefault();
@@ -42,7 +53,7 @@ document.onkeydown = (e) => {
     // CTRL+ENTER to submit when editing
     if ((e.ctrlKey || e.metaKey) && e.keyCode == 13) {
       e.preventDefault();
-      return document.querySelector("#form").submit();
+      return $("#form").submit();
     }
   } else if (name != "INPUT" && name != "TEXTAREA") {
     // ignore keydown if input or textarea is focused
@@ -75,7 +86,7 @@ document.onkeydown = (e) => {
     // ctrl-n new / i insert
     if (e.key == "i" || (e.ctrlKey && e.key == "n")) {
       e.preventDefault();
-      let newLink = document.querySelector("#new-link");
+      let newLink = $("#new-link");
       if (newLink) {
         return (window.location = newLink.href);
       } else {
@@ -84,7 +95,7 @@ document.onkeydown = (e) => {
     }
 
     // ctrl-e edit / i insert mode
-    var editLink = document.querySelector("#edit-link");
+    var editLink = $("#edit-link");
     if (editLink && ((e.ctrlKey && e.key == "e") || e.key == "i")) {
       e.preventDefault();
       return (window.location = editLink.href);
@@ -96,9 +107,10 @@ document.onkeydown = (e) => {
   }
 };
 
+/* jump to page (ctrl-j) */
 window.WIKI_PAGES = [];
 var first = true;
-document.querySelectorAll("#jump-list li a").forEach((el) => {
+$$("#jump-list li a").forEach((el) => {
   if (first) {
     el.closest("li").classList.add("active");
     first = false;
@@ -109,11 +121,10 @@ document.querySelectorAll("#jump-list li a").forEach((el) => {
   });
 });
 
-// jump to page (ctrl-j)
-document.querySelector("#jump-pattern").addEventListener("keydown", (e) => {
+$$("#jump-pattern").forEach((el) => el.addEventListener("keydown", (e) => {
   // down arrow or ctrl-n
   if (e.keyCode == 40 || (e.keyCode == 78 && e.ctrlKey)) {
-    let el = document.querySelector("#jump-list .active");
+    let el = $("#jump-list .active");
     var next = el.nextElementSibling;
     while (next && !next.offsetParent) next = next.nextElementSibling;
     if (next && next.offsetParent) {
@@ -127,7 +138,7 @@ document.querySelector("#jump-pattern").addEventListener("keydown", (e) => {
 
   // up arrow or ctrl-p
   if (e.keyCode == 38 || (e.keyCode == 80 && e.ctrlKey)) {
-    let el = document.querySelector("#jump-list .active");
+    let el = $("#jump-list .active");
     var prev = el.previousElementSibling;
     while (prev && !prev.offsetParent) prev = prev.previousElementSibling;
     if (prev && prev.offsetParent) {
@@ -141,14 +152,14 @@ document.querySelector("#jump-pattern").addEventListener("keydown", (e) => {
 
   // enter (open page/tag)
   if (e.keyCode == 13) {
-    window.location = document.querySelector("#jump-list .active a").href;
+    window.location = $("#jump-list .active a").href;
     e.preventDefault();
     return false;
   }
 
   const fuse = new Fuse(window.WIKI_PAGES, { keys: ["name"] });
   const pattern = e.target.value;
-  let list = document.querySelectorAll("#jump-list li");
+  let list = $$("#jump-list li");
   if (pattern == "") {
     list.forEach((el) => (el.style.display = ""));
   } else {
@@ -156,14 +167,14 @@ document.querySelector("#jump-pattern").addEventListener("keydown", (e) => {
     list.forEach((el) => (el.style.display = "none"));
     for (var i = matches.length - 1; i >= 0; i--) {
       let match = matches[i];
-      let el = document.querySelector("#jump-" + match.refIndex);
-      let jumpList = document.querySelector("#jump-list");
+      let el = $("#jump-" + match.refIndex);
+      let jumpList = $("#jump-list");
       jumpList.removeChild(el);
-      var active = document.querySelector(".active");
+      var active = $(".active");
       if (active) active.classList.toggle("active");
       el.classList.add("active");
       jumpList.insertBefore(el, jumpList.childNodes[0]);
       el.style.display = "";
     }
   }
-});
+}));
